c198efda8e463b1252451fb26c1eda03e0250bcf
ilham <dwiilhamm026@gmail.com>
2025-03-26 15:06:38 +0700

    Add parameter handling in Outlet component

--- src/libs/route/outlet.tsx
+++ src/libs/route/outlet.tsx
@@ -1,7 +1,10 @@
 // src/libs/route/outlet.tsx
 
 import { getLayout } from "./layout";
+import { data } from "./routes";
 
 export const Outlet = () => {
+  let path = data.params[data.position];
+  if (!path) return;
   return getLayout();
 };
