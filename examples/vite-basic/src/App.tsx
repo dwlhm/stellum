b1da0e06e19c5b7bdcc3a02b7e50eb0b7d459285
Dwi Ilham Maulana <dwlhm@Dwis-MacBook-Air.local>
2025-05-20 18:06:10 +0700

    fix: cannot use stellum in examples project

--- examples/vite-basic/src/App.tsx
+++ examples/vite-basic/src/App.tsx
@@ -3,6 +3,7 @@ import { createRouter } from "stellum";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Counter } from "./counter"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,7 +12,7 @@ function App() {
     {
       route: {
         "/": {
-          layout: () => <div>Home</div>,
+          layout: () => <div>Home<Counter /></div>,
           child: {
             "/about": {
               layout: () => <div>About</div>,
@@ -61,26 +62,6 @@ function App() {
   return (
     <>
       <Router />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
